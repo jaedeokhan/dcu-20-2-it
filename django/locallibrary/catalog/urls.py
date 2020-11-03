df50323cb7e8037b0d6c974ba30e5c6026848ac7
jaedeokhan <dgd03023@cu.ac.kr>
2020-11-03 11:42:15 +0900

    add 2020 1103 TUE

--- django/locallibrary/catalog/urls.py
+++ django/locallibrary/catalog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from catalog import views
+import uuid
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
+
